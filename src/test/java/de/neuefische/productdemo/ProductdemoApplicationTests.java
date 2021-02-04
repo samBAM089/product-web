fadaccba216e7df5c34406e2f7922da6d3e123f0
Samuel N <samuel.ngo@me.com>
2021-02-04 09:27:00 +0100

    Add comments

--- src/test/java/de/neuefische/productdemo/ProductdemoApplicationTests.java
+++ src/test/java/de/neuefische/productdemo/ProductdemoApplicationTests.java
@@ -6,6 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ProductdemoApplicationTests {
 
+
+
+
 	@Test
 	void contextLoads() {
 	}
---
+++ src/test/java/de/neuefische/productdemo/controller/OrderControllerTest.java
@@ -0,0 +1,11 @@
+package de.neuefische.productdemo.controller;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+class OrderControllerTest {
+
+}
